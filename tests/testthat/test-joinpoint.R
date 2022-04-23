1c7a062673f84ae123cd30e32ea2046638bf8159
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-04-23 16:40:35 +0200

    Update test-joinpoint.R

--- tests/testthat/test-joinpoint.R
+++ tests/testthat/test-joinpoint.R
@@ -1,6 +1,6 @@
 
 
-test_that("joinpoint", {
+test_that("joinpoint: default", {
     skip_on_cran()
 
     run_opt = run_options(model="ln", max_joinpoints=2, n_cores=3)
@@ -8,19 +8,27 @@ test_that("joinpoint", {
 
     #by, SE
     jp = joinpoint(sample_data, x=year, y=rate, by=sex, se=se,
-                   run_opt_ini=run_opt, export_opt_ini=export_opt)
+                   run_opts=run_opt, export_opts=export_opt)
     expect_type(jp, "list")
+
+    p = jp_plot(jp, ncol=1)
+    expect_type(p, "list")
+    expect_s3_class(p, "patchwork")
 })
 
 
-test_that("joinpoint no BY, no SE", {
+test_that("joinpoint: linear, no BY, no SE", {
     skip_on_cran()
 
-    run_opt = run_options(model="ln", max_joinpoints=2, n_cores=3)
+    run_opt = run_options(model="lin", max_joinpoints=2, n_cores=3)
     export_opt = export_options()
 
     #no by, no SE
     jp = joinpoint(sample_data, x=year, y=rate,
-                   run_opt_ini=run_opt, export_opt_ini=export_opt)
+                   run_opts=run_opt, export_opts=export_opt)
     expect_type(jp, "list")
+
+    p = jp_plot(jp)
+    expect_type(p, "list")
+    expect_s3_class(p, "patchwork")
 })
